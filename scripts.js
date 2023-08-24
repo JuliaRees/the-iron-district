76693616eb488fb66fccef0d0345b89526aee400
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-08-24 15:00:19 +0800

    favicon

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function() {
     
     $('.hamburger-button').click(function(){
         $('.mobile-menu').slideToggle(250);
         $(this).toggleClass('active');
     });
 
-})
+    // Close mobile menu when the close button is clicked
+    $('.mobile-menu .close-button').click(function() {
+        $('.mobile-menu').slideUp(250);
+        $('.hamburger-button').removeClass('active');
+    });
+
+    // Close mobile menu when a link is clicked
+    $('.mobile-menu a').click(function() {
+        $('.mobile-menu').slideUp(250);
+        $('.hamburger-button').removeClass('active');
+    });
+
+});
+
+// Animate on scroll
+AOS.init();
